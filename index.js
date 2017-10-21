7a2c6b737a69fae892d44c5e0664ea3e567329de
Dennis Durairaj <dennis.durairaj@gmail.com>
2017-10-21 19:44:42 +0200

    edit loaderio file fix

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var app = express()
 
 app.get('/', function (req, res) {
   res.send('Hello Digital Ocean!')
+});
+
+app.get('/loaderio-5b2dec67e518d3d7d815c0db86b65b60', function (req, res) {
+  res.send('loaderio-5b2dec67e518d3d7d815c0db86b65b60')
 })
 
 app.listen(3000, function () {
